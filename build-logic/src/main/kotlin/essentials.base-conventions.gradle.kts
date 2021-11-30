EssentialsX/Essentials

Update Spigot to 1.18

build-logic/src/main/kotlin/essentials.base-conventions.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 val baseExtension = extensions.create<EssentialsBaseExtension>("essentials", project)
 
 val checkstyleVersion = "8.36.2"
-val spigotVersion = "1.17.1-R0.1-SNAPSHOT"
+val spigotVersion = "1.18-pre5-R0.1-SNAPSHOT"
 val junit5Version = "5.7.0"
 val mockitoVersion = "3.2.0"
 

rewsdjad1/EssentialsS

build-logic/src/main/kotlin/essentials.base-conventions.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 val baseExtension = extensions.create<EssentialsBaseExtension>("essentials", project)
 
 val checkstyleVersion = "8.36.2"
-val spigotVersion = "1.17.1-R0.1-SNAPSHOT"
+val spigotVersion = "1.18-rc3-R0.1-SNAPSHOT"
 val junit5Version = "5.7.0"
 val mockitoVersion = "3.2.0"
 