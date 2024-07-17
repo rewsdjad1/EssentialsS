dc35bc43d34a5dff3548c68d50a72715005353e7
Euphyllia <bierque.euphyllia@gmail.com>
2024-07-17 02:15:15 +0200

    Delete Essentials-Patchs directory

--- Essentials-Patchs/providers/BaseProviders/src/main/java/net/ess3/provider/BiomeKeyProvider.java
+++
@@ -1,8 +0,0 @@
-package net.ess3.provider;
-
-import org.bukkit.NamespacedKey;
-import org.bukkit.block.Block;
-
-public interface BiomeKeyProvider extends Provider {
-    NamespacedKey getBiomeKey(Block block);
-}
--- Essentials-Patchs/providers/BaseProviders/src/main/java/net/ess3/provider/PlayerLocaleProvider.java
+++
@@ -1,7 +0,0 @@
-package net.ess3.provider;
-
-import org.bukkit.entity.Player;
-
-public interface PlayerLocaleProvider extends Provider {
-    String getLocale(Player player);
-}
